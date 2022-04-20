21e3f185f521246023e4c50910923fbf02d39be4
aerobull42 <doshinaman2006@gmail.com>
2022-04-20 18:47:45 +1000

    initial push

---
+++ plan.md
@@ -0,0 +1,11 @@
+### COlOURS
+
+Black: #0b0a07
+White: #F0EFE6
+Grey: #CED4DA
+Main: #0E67B4
+Accent: #7ABCF5
+
+### NAV
+
+Logo, AquaAI, Problem, Mission, Solution, Join the beta!
---
+++ script.js
@@ -0,0 +1,30 @@
+var element = document.getElementById("spin");
+text = ["dolphins", "whales", "turtles", "seabirds"];
+element.innerHTML = text[0];
+let timer = 0;
+
+setInterval(function () {
+  element.innerHTML = text[timer];
+  element.className = "";
+  if (timer % 4 == 0) {
+    element.classList.add("dolphins");
+  }
+  if (timer % 4 == 1) {
+    element.classList.add("whales");
+  }
+  if (timer % 4 == 2) {
+    element.classList.add("turtles");
+  }
+  if (timer % 4 == 3) {
+    element.classList.add("seabirds");
+  }
+  if (timer == 3) {
+    timer = 0;
+  } else {
+    timer++;
+  }
+}, 2500);
+
+function fadeInUp(elem) {}
+
+function fadeOutUp(elem) {}
