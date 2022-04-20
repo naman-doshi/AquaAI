e7e42f8c541a2e20b9212db4a74b214f1f360833
aerobull42 <doshinaman2006@gmail.com>
2022-04-20 22:13:42 +1000

    updated cs

--- cs.js
+++ cs.js
@@ -1,14 +1,20 @@
-var counter = { var: 0 };
-var tal = document.getElementById("cx1");
-
-TweenMax.to(counter, 1, {
-  var: 1600000,
-  onUpdate: function () {
-    tal.innerHTML = numberWithCommas(Math.ceil(counter.var));
-  },
-  ease: Circ.easeOut,
-});
-
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+sleep(3000).then(() => {
+  var counter = { var: 0 };
+  var tal = document.getElementById("cx1");
+
+  TweenMax.to(counter, 1, {
+    var: 1600000,
+    onUpdate: function () {
+      tal.innerHTML = numberWithCommas(Math.ceil(counter.var));
+    },
+    ease: Circ.easeOut,
+  });
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+});
